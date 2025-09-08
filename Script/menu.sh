75afc692c7fd77c940ac961183aa2fdf5127b9ca
Your Name <you@example.com>
2025-09-08 17:32:00 +0800

    SHAO ubuntu

--- Script/menu.sh
+++ Script/menu.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-if ps -a | grep -q 'rofi' ;
-then 
-	hyprctl dispatch "exec [noanim] pkill rofi" 
+if ps -a | grep -q 'rofi'; then
+  hyprctl dispatch "exec [noanim] pkill rofi"
 else
-	hyprctl dispatch "exec [noanim] rofi -show combi"
+  hyprctl dispatch "exec [noanim] rofi -theme-str 'element-icon { size: 40px;}' -show combi"
 fi
-
