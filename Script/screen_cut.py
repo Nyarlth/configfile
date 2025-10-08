834b4190c978fbaa1424627ecb122673890559c5
Your Name <you@example.com>
2025-10-09 07:03:03 +0800

    SHAO update

--- Script/screen_cut.py
+++ Script/screen_cut.py
@@ -2,14 +2,21 @@
 
 import os
 import datetime
-
-filename = str(datetime.datetime.now()).split('.')[0]
-
+import re
+filename = str(datetime.datetime.now()).split('.')[0].replace(' ','_')
+patern = "(.*),(.*) (.*)x(.*)"
 size = os.popen('slurp', 'r').read().replace("\n","")
-
-os.system(f'grim -g "{size}" ~/Pictures/Capture/"{filename}.png"')
+z = re.match(patern,size)
+x,y = int(z.group(1)), int(z.group(2))
+_x,_y = int(z.group(3)),int(z.group(4))
+x+=2
+y+=2
+_x -=4 
+_y -=4
+os.system(f'grim -g "{x},{y} {_x}x{_y}" ~/Pictures/Capture/"{filename}.png"')
 
 
 os.system(f'wl-copy < ~/Pictures/Capture/"{filename}".png')
+os.system(f'echo ~/Pictures/Capture/"{filename}".png > ~/Pictures/Capture/.cache')
 
 os.system("notify-send 截图一张")
